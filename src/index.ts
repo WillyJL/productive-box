155058756b78e6a755e7261e51da9dfc51513289
WillyJL <49810075+Willy-JL@users.noreply.github.com>
2021-09-22 20:48:34 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ interface IRepo {
     files: {
       [filename]: {
         // eslint-disable-next-line quotes
-        filename: (morning + daytime) > (evening + night) ? "I'm an early 🐤" : "I'm a night 🦉",
+        filename: (morning + daytime) > (evening + night) ? "⌚ I'm an early 🐤" : "⌚ I'm a night 🦉",
         content: lines.join('\n'),
       },
     },
